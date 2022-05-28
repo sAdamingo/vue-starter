4f03ca1482967ccb4e57eee6b3b9a056abdc15ea
Adam.Stelmach <adam.stelmach@fdmgroup.com>
2022-05-28 14:42:02 +0200

    Beautiful appearance!

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,23 @@
   <div>
     <div v-if="isLogged">
         <h1>Twój e-mail to: {{email}}</h1>
-      <h2>{{isLogged}}</h2>
-        <div v-if="email.length < 10">Ale masz krótki adres!</div>
 
+        <div v-if="email.length < 10">Ale masz krótki adres!</div>
         <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
         <div class="warn" v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
         <input type="email" v-model="email">
         <button @click="logIn()">Wyświetl mój e-mail w alercie</button>
         </div>
     <div v-else>
         <h1>Witaj {{email}}</h1>
-        <a @click="logIn()">Wyloguj!</a>
+        <button class="logOut" @click="logIn()">Wyloguj!</button>
     </div>
   </div>
 </template>
 
 <script>
-
+import 'milligram';
 export default {
   data() {
     return {
@@ -41,5 +41,9 @@ export default {
 .warn {
   color:red;
   font-size: large;
+}
+.logOut {
+  color: blue;
+  text-underline-mode: true;
 }
 </style>
